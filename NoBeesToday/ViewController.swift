3798cfba0c5b542e552854c5533ab45ad0a3b46e
mkanzler <moritzkanzler@googlemail.com>
2016-05-28 13:44:02 +0200

    Initial Commit

---
+++ NoBeesToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoBeesToday
+//
+//  Created by Moritz Kanzler on 28.05.16.
+//  Copyright © 2016 Moritz Kanzler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
